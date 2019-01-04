8ae24aec12516e1b95377125ff3f9c4d4f9b7070
Flooorent <florent.moiny@gmail.com>
2019-01-04 11:08:38 +0100

    fix: use correct routes

--- models/book.js
+++ models/book.js
@@ -13,7 +13,7 @@ var BookSchema = new Schema({
 BookSchema
     .virtual('url')
     .get(function() {
-        return `/catalog/book/${this._id}`
+        return `/book/${this._id}`
     })
 
 module.exports = mongoose.model('Book', BookSchema)
